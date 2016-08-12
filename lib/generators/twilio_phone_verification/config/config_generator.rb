1f88d0f97b959a51e2f27bf04db666fc4828aeb3
vishgleb@mail.ru <vishgleb@mail.ru>
2016-08-12 17:41:57 +0300

    Fixes with generator

--- lib/generators/twilio_phone_verification/config/config_generator.rb
+++ lib/generators/twilio_phone_verification/config/config_generator.rb
@@ -1,4 +1,4 @@
-class TwilioPhoneVerificationGenerator < Rails::Generators::Base
+class ConfigGenerator < Rails::Generators::Base
   source_root(File.expand_path(File.dirname(__FILE__))
   def copy_initializer
     copy_file 'twilio_phone_verification.rb', 'config/initializers/twilio_phone_verifications.rb'
